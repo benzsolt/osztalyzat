2e4dd184bf1f48b431f7d3078e9d444395edc463
benzsolt <57367149+benzsolt@users.noreply.github.com>
2019-11-11 11:49:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace osztalyzat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adja meg az elért pontszámot: ");
+            int pontSzam = int.Parse(Console.ReadLine());
+
+            if (pontSzam < 0 || pontSzam > 100)
+            {
+                Console.WriteLine("A bevitt adat hibás!");
+            }
+            else if (pontSzam > 0 && pontSzam <= 50)
+            {
+                Console.WriteLine("Elégtelen");
+            }
+            else if (pontSzam > 50 && pontSzam <= 65)
+            {
+                Console.WriteLine("Elégséges");
+            }
+            else if (pontSzam > 65 && pontSzam <= 80)
+            {
+                Console.WriteLine("Közepes");
+            }
+            else if (pontSzam > 80 && pontSzam <= 90)
+            {
+                Console.WriteLine("Jó");
+            }
+            else
+            {
+                Console.WriteLine("Jeles");
+            }
+            Console.ReadKey();
+        }
+    }
+}
